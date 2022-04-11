b52996cf441963626d31e153fa3d54a0ff2e045e
Cleo Aguiar <cleotaisas@gmail.com>
2022-04-11 20:15:17 -0300

    feat: add percentage bachelors equation

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -12,7 +12,9 @@ def calculate_demographic_data(print_data=True):
     average_age_men = df['age'].where(df['sex'] == 'Male').mean().round(1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    bachelor_degree = df['education'].where(df['education'] == 'Bachelors').value_counts()
+    all_degree = df['education'].count()
+    percentage_bachelors = (100 *  bachelor_degree / all_degree).round(1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
